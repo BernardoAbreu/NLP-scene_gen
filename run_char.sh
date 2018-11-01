09a4060801d0c3754accc98df56d691f17f5a642
bernardoabreu <bernardoaabreu@gmail.com>
2018-11-01 19:18:58 -0200

    Add tags

--- run_char.sh
+++ run_char.sh
@@ -27,31 +27,38 @@ END=1
 # done
 
 # mkdir $OUTPUT_FILE
+# sed -i -E "s/ {5,}[A-Z][0-9]+[A-Z]?$//g" $FILE;
 
+for FILE in $INPUT_DIR/*; do
+	BASEFILE=$(basename $FILE);
+	echo $BASEFILE;
+	sed -i -E "s/^( *)[A-Z][0-9]+[A-Z]? +(.*[^ ]) +[A-Z][0-9]+[A-Z]?$/\1\2/g" $FILE;
+	sed -i -E "/^((.*\(MORE\) *?)|(.*CONTINUED.*)|( *\(CONT\.?\).*)|( *[0-9]{1,3}\.))$/d" $FILE;
+	sed -i -E "/<\!--/,/-->/d" $FILE;
+	sed -i -E "/<b>/,/<\/b>/d" $FILE;
+	sed -i -E "/<\/?b>/d" $FILE;
+	sed -i '/^\s*$/d' $FILE;
+	sed -i -E 's/\s+\*\s*$//g' $FILE;
+done
 
 # for FILE in $INPUT_DIR/*; do
 # 	BASEFILE=$(basename $FILE);
 # 	echo $BASEFILE;
-
-# 	sed -E "/^((.*\(MORE\) *?)|(.*CONTINUED.*)|( *\(CONT\.?\).*)|( *[0-9]{1,3}\.))$/d" $FILE > $OUTPUT_FILE/$BASEFILE
-# 	sed -i -E "/<\!--/,/-->/d" $OUTPUT_FILE/$BASEFILE;
-# 	sed -i -E "/<b>/,/<\/b>/d" $OUTPUT_FILE/$BASEFILE;
-# 	sed -i -E "/<\/?b>/d" $OUTPUT_FILE/$BASEFILE;
-# 	sed -i -E 's/\s+\*\s*$//g' $OUTPUT_FILE/$BASEFILE;
-# 	sed -i -E "s/((FADE|DISSOLVE|CUT) [A-Z ]*)(:)?/\1:\n/g" $OUTPUT_FILE/$BASEFILE;
-# 	sed -i '/^\s*$/d' $OUTPUT_FILE/$BASEFILE;
-	sed -i -E "s/^[ \t]+([A-Z][A-Z\'\"\.0-9 ]*) *\(([a-zA-Z0-9\'\"\. ]+)\)?$/<CHAR>__{\1} <CHAR_PAR>__{\2}/" $OUTPUT_FILE/$BASEFILE;
-# 	sed -i -E "s/^(INT|EXT|[0-9]+)(.*)$/<LOC>__{\1\2}/" $OUTPUT_FILE/$BASEFILE;
-# 	sed -i -E "s/^ *([A-Z ]+:)$/<TRANS>__{\1}/" $OUTPUT_FILE/$BASEFILE;
-# 	sed -i -E "s/^ {2,}(.*)$/<DIAL>__{\1}/" $OUTPUT_FILE/$BASEFILE;
+# 	sed -E "s/((FADE|DISSOLVE|CUT) [A-Z ]*)(:)?/\1:\n/g" $FILE > $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/^[ \t]+([A-Z][A-Z\'\"\-\.0-9 ]*) *(\([a-zA-Z0-9\'\"\. ]+\))?$/<CHAR>__{\1} <CHAR_PAR>__{\2}/g" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/^(INT|EXT|[0-9]+)(.*)$/<LOC>__{\1\2}/g" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/^ *([A-Z ]+:)$/<TRANS>__{\1}/g" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/^ {5,}((\(.*)|([^\)]+\)))$/<DIAL_PAR>__{\1}/g" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/^ {5,}(.*)$/<DIAL>__{\1}/g" "${OUTPUT_FILE}/${BASEFILE}";
+# 	sed -i -E "s/^([A-Z ]+)$/<ACTION_ALT>__{\1}/g" "${OUTPUT_FILE}/${BASEFILE}";
+# 	sed -i -E "s/^([^<].*)+$/<ACTION>__{\1}/g" "${OUTPUT_FILE}/${BASEFILE}";
 # done
 
-
-rm "locs_.txt";
-for FILE in $INPUT_DIR/*; do
-	# echo $(basename $FILE);
-	# ./scene_split.py $FILE >> "${OUTPUT_FILE}_${END}.txt"
-		echo $(basename $FILE) >> "locs_.txt";
-		cat $FILE | egrep "^<TRANS>__.*?$" >> "locs_.txt";
-	# echo "${OUTPUT_FILE}_${END}.txt"
-done
+# rm "locs_.txt";
+# for FILE in $INPUT_DIR/*; do
+# 	# echo $(basename $FILE);
+# 	# ./scene_split.py $FILE >> "${OUTPUT_FILE}_${END}.txt"
+# 		echo $(basename $FILE) >> "locs_.txt";
+# 		cat $FILE | egrep "^<TRANS>__.*?$" >> "locs_.txt";
+# 	# echo "${OUTPUT_FILE}_${END}.txt"
+# done

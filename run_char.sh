4041cbd233fa1f313ae8a2ad29adc46507bffb6e
Bernardo Abreu <bernardoaabreu@gmail.com>
2018-11-01 03:04:38 -0300

    Add regex to process files

--- run_char.sh
+++ run_char.sh
@@ -2,7 +2,7 @@
 
 INPUT_DIR=$1
 
-OUTPUT_FILE="character_list"
+OUTPUT_FILE="changes"
 
 INDEX=0
 END=1
@@ -10,31 +10,48 @@ END=1
 
 # for m in movies/*; do ./scene_split.py $m inter/$(basename $m) ; done
 
-rm "${OUTPUT_FILE}_"*
+# rm "${OUTPUT_FILE}_"*
 
+# for FILE in $INPUT_DIR/*; do
+# 	# echo $(basename $FILE);
+# 	# ./scene_split.py $FILE >> "${OUTPUT_FILE}_${END}.txt"
+# 	if [ "${END}" = "9" ]; then
+# 		echo $(basename $FILE) >> "${OUTPUT_FILE}_${END}.txt"
+# 		cat $FILE | egrep "^[ \t]+([A-Z]\.?([A-Z\'\"0-9 ]|[A-Z\.])+)( +\([a-zA-Z0-9\(\)\'\"\. ]+\))?$" >> "${OUTPUT_FILE}_${END}.txt"
+# 	fi;
+# 	# echo "${OUTPUT_FILE}_${END}.txt"
+# 	let INDEX=${INDEX}+1
+# 	if [ "${INDEX: -2}" = "00" ]; then
+# 		let END=${END}+1
+# 	fi;
+# done
+
+# mkdir $OUTPUT_FILE
+
+
+# for FILE in $INPUT_DIR/*; do
+# 	BASEFILE=$(basename $FILE);
+# 	echo $BASEFILE;
+
+# 	sed -E "/^((.*\(MORE\) *?)|(.*CONTINUED.*)|( *\(CONT\.?\).*)|( *[0-9]{1,3}\.))$/d" $FILE > $OUTPUT_FILE/$BASEFILE
+# 	sed -i -E "/<\!--/,/-->/d" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "/<b>/,/<\/b>/d" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "/<\/?b>/d" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E 's/\s+\*\s*$//g' $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/((FADE|DISSOLVE|CUT) [A-Z ]*)(:)?/\1:\n/g" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i '/^\s*$/d' $OUTPUT_FILE/$BASEFILE;
+	sed -i -E "s/^[ \t]+([A-Z][A-Z\'\"\.0-9 ]*) *\(([a-zA-Z0-9\'\"\. ]+)\)?$/<CHAR>__{\1} <CHAR_PAR>__{\2}/" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/^(INT|EXT|[0-9]+)(.*)$/<LOC>__{\1\2}/" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/^ *([A-Z ]+:)$/<TRANS>__{\1}/" $OUTPUT_FILE/$BASEFILE;
+# 	sed -i -E "s/^ {2,}(.*)$/<DIAL>__{\1}/" $OUTPUT_FILE/$BASEFILE;
+# done
+
+
+rm "locs_.txt";
 for FILE in $INPUT_DIR/*; do
 	# echo $(basename $FILE);
 	# ./scene_split.py $FILE >> "${OUTPUT_FILE}_${END}.txt"
-	if [ "${END}" = "9" ]; then
-		echo $(basename $FILE) >> "${OUTPUT_FILE}_${END}.txt"
-		cat $FILE | egrep "^[ \t]+([A-Z]\.?([A-Z\'\"0-9 ]|[A-Z\.])+)( +\([a-zA-Z0-9\(\)\'\"\. ]+\))?$" >> "${OUTPUT_FILE}_${END}.txt"
-	fi;
+		echo $(basename $FILE) >> "locs_.txt";
+		cat $FILE | egrep "^<TRANS>__.*?$" >> "locs_.txt";
 	# echo "${OUTPUT_FILE}_${END}.txt"
-	let INDEX=${INDEX}+1
-	if [ "${INDEX: -2}" = "00" ]; then
-		let END=${END}+1
-	fi;
-done
-
-
-
-for FILE in $INPUT_DIR/*; do
-	BASEFILE=$(basename $FILE);
-	echo $BASEFILE;
-	sed -E "/^((.*\(MORE\) *?)|( *\(CONT\.?\).*)|( *[0-9]{1,3}\.))$/d" $FILE > changes/$BASEFILE
-	sed -i 's/ +\*?$//' changes/$BASEFILE;
-	sed -i '/^\s*$/d' changes/$BASEFILE;
-	sed -E -i "s/^[ \t]+([A-Z][A-Z\'\"\.0-9 ]*) *(\([a-zA-Z0-9\'\"\. ]+\))?$/<CHAR>__{\1} <CHAR_PAR>__{\2}/" changes/$BASEFILE;
-	sed -E -i "s/^(INT|EXT|[0-9]+)(.*)$/<LOC>__{\1\2}/" changes/$BASEFILE;
-	# sed -E"s/^[ \t]+([A-Z][A-Z\'\"\.0-9 ]*) *(\([a-zA-Z0-9\'\"\. ]+\))?$/<CHAR>__{\1} <CHAR_PAR>__{\2}/" $FILE > chars/$BASEFILE;
 done

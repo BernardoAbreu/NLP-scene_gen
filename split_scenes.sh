7f6e8de8d9288cd7b59ec1103cdaff5c74ce1a87
bernardoabreu <bernardoaabreu@gmail.com>
2018-11-05 02:35:13 -0200

    Add script to split scenes

---
+++ split_scenes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+INPUT_DIR=$1
+
+OUTPUT_DIR="scenes"
+
+INDEX=0
+END=1
+
+
+mkdir -p $OUTPUT_FILE
+
+for FILE in $INPUT_DIR/*; do
+	BASEFILE=$(basename $FILE);
+	echo $BASEFILE;
+	mkdir -p "${OUTPUT_DIR}/${BASEFILE}";
+	csplit -z -f "${OUTPUT_DIR}/${BASEFILE}/" $FILE "/<LOC>__.*/" {*};
+done
